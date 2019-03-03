dd1babf26373dab7b351adf1983ca4db6a2aeb53
Sachin Ahuja <sachinahj@gmail.com>
2019-03-03 15:34:56 -0800

    update migrate script

--- migrate.sh
+++ migrate.sh
@@ -1,26 +1,19 @@
 export TS_ENV=${1:-'dev'}
 twitch_stitch_root="${PWD%/*}"
-migrations_dir=$PWD/migrations
 
-echo "TS_ENV: $TS_ENV"
-echo "\n"
-echo "start migrations"
-
-cd $migrations_dir
+echo "migrate | start | twitch-stitch-$TS_ENV"
+cd $twitch_stitch_root/ts_infra/migrations
 if [ ! -d ./venv ]; then
+    echo "migrate | bootstrapping | twitch-stitch-$TS_ENV"
     rm -rf ./venv
     rm -rf ./__pycache__
     /usr/local/Cellar/python/3.6.5_1/bin/python3 -m venv ./venv
     source venv/bin/activate
-    pip3 install -r requirements.txt
-    cd $twitch_stitch_root/ts_shared
-    pip3 install -e ./ts_config
+    pip3 install --process-dependency-links -r requirements.txt
+    pip3 install --process-dependency-links -e $twitch_stitch_root/ts_shared/ts_config
     deactivate
-    cd $migrations_dir
 fi
 
+echo "migrate | migrating | twitch-stitch-$TS_ENV"
 source venv/bin/activate
 alembic upgrade head
-
-echo "done migrations"
-echo "\n"
