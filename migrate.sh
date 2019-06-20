ee12b5ab1aff9c4e6d05b2e54d23741a0c78bd05
Sachin Ahuja <sachinahj@gmail.com>
2019-06-20 01:03:13 -0700

    3.7

--- migrate.sh
+++ migrate.sh
@@ -1,14 +1,16 @@
+# migrate
 ts_env=${1:-'dev'}
 echo "migrate | start | ts_env=$ts_env"
 
 twitch_stitch_root=${PWD%/*}
 cd $twitch_stitch_root/ts_infra/migrations
 if [ ! -d ./venv ]; then
-    echo "migrate | bootstrapping | ts_env=$ts_env"
+    echo "migrate | bootstrapping"
     rm -rf ./venv
     rm -rf ./__pycache__
-    /usr/local/Cellar/python/3.6.5_1/bin/python3 -m venv ./venv
+    python3 -m venv ./venv
     source venv/bin/activate
+    python3 -m pip install pip==18.1
     pip3 install --process-dependency-links -r requirements.txt
     pip3 install --process-dependency-links -e $twitch_stitch_root/ts_shared/ts_config
     deactivate
